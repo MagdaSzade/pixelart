11edfc4ddcb885328bd5ac9a5cdb6320beec04a2
MagdaSzade <magdalenasuchanek@gmail.com>
2020-01-20 22:48:30 +0100

    draft

---
+++ server/controllers/userCtrl.js
@@ -0,0 +1,29 @@
+
+
+createUser = (req, res) => {
+
+}
+
+getAllUsers = (req, res) => {
+
+}
+
+getUserByUsername = (req, res) => {
+
+}
+
+login = (req, res) => {
+
+}
+
+deleteUserById = (req, res) => {
+
+}
+
+module.exports = {
+    createUser,
+    getAllUsers,
+    getUserByUsername,
+    login,
+    deleteUserById 
+}
---
+++ server/routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const userCtrl = require('../controllers/userCtrl');
+
+const router = express();
+
+router.post('/register', userCtrl.createUser);
+router.get('/', userCtrl.getAllUsers);
+router.get('/:username', userCtrl.getUserByUsername);
+router.get('/login', userCtrl.login);
+router.delete('/:id', userCtrl.deleteUserById);
