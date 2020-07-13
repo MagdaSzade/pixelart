14d13eee3eea485b84bcf667d319eec7e7baa711
MagdaSzade <magdalenasuchanek@gmail.com>
2020-07-13 17:44:20 +0200

    add link to siepomaga.pl

--- client/src/components/PopUp.js
+++ client/src/components/PopUp.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
+import FacebookIcon from '@material-ui/icons/Facebook';
 import Button from './Button';
 
 import '../styles/popup.css';
-import { ThemeProvider } from '@material-ui/core';
 
 class PopUp extends React.Component {
+    state = {
+        isFacebook: false
+    }
     componentDidMount() {
-        var canvas = document.getElementById("myCanvas");
-        var ctx = canvas.getContext("2d");
+        const canvas = document.getElementById("myCanvas");
+        const ctx = canvas.getContext("2d");
         this.props.pixels.forEach((pixel, index) => {
             ctx.fillStyle = pixel.color;
             const startPointY = parseInt(pixel.key.substring(0, 2)) * 10;
             const startPointX = parseInt(pixel.key.substring(2)) * 10;
             ctx.fillRect(startPointX, startPointY, 10, 10);
+            setTimeout(() => {this.setState({isFacebook: true})}, 10000)
         })
-        
+    }
+
+    openPortal = () => {
+        const win = window.open( 'https://www.siepomaga.pl/skarbonki/pixelart/koszyk/dodaj' ,'_blank');
+        win.focus();
+    }
+
+    isFacebookAvailable() {
+        if (this.state.isFacebook) {
+            return <FacebookIcon />
+        }
     }
 
     render() {
@@ -26,9 +40,10 @@ class PopUp extends React.Component {
                         <canvas className="solid-border" id="myCanvas" width={this.props.width*10} height={this.props.height*10}></canvas>
                     </div>
                     <div className="summary">
-                        Aby udostępnić swój pixelar zapłać {this.props.width * this.props.height}gr na skarbonkę
-                        <Button />
+                        Aby udostępnić swój pixelart na facebooku zapłać cokolwiek na skarbonkę dla Sandry.
+                        <Button onButtonClick={this.openPortal} text="wpłać"/>
                     </div>
+                    {this.isFacebookAvailable()}
                 </div>
             </div>
         )
