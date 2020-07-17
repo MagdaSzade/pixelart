75a456880a8fe2a0f6ea8b4bb3392caae4c07b7b
MagdaSzade <magdalenasuchanek@gmail.com>
2020-07-17 21:18:37 +0200

    protect save art from empty req

--- server/controllers/artCtrl.js
+++ server/controllers/artCtrl.js
@@ -2,23 +2,25 @@ const { Art, artValidator } = require('../models/artModel');
 
 
 saveArt = async (req, res) => {
-    const reqArt = {
-        pixels: req.body.pixels,
-        width: req.body.width
-    };
-    if (!artValidator(reqArt)) {
-        return res.status(400).send();
-    };
-    let art = new Art({
-        pixels: reqArt.pixels,
-        width: reqArt.width
-    });
-    try {
-        art = await art.save();
-    } catch(err) {
-        return res.status(500).send(err.massage[0]);
-    }
-    return res.status(201).send(art);
+    if(req.body.pixels && req.body.width) {
+        const reqArt = {
+            pixels: req.body.pixels,
+            width: req.body.width
+        };
+        if (!artValidator(reqArt)) {
+            return res.status(400).send();
+        };
+        let art = new Art({
+            pixels: reqArt.pixels,
+            width: reqArt.width
+        });
+        try {
+            art = await art.save();
+        } catch(err) {
+            return res.status(500).send(err.massage[0]);
+        }
+        return res.status(201).send(art);
+    } return res.status(400).send();
 }, 
 
 isPayed = async (req, res) => {
